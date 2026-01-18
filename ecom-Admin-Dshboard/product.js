73feda76a4881132b5c08cda6934d50d4a912d66
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2026-01-18 17:09:39 +0500

    i

--- ecom-Admin-Dshboard/product.js
+++ ecom-Admin-Dshboard/product.js
@@ -23,7 +23,7 @@ async function getAllCat() {
             }
             allCat = Object.values(snap.val())
             console.log(allCat)
-            for(var i = 0;i<allCat.length ; i++){
+            for (var i = 0; i < allCat.length; i++) {
                 console.log(allCat[i])
                 catSelect.innerHTML += `
                 <option value = ${allCat[i].catName}>${allCat[i].catName} </option>
@@ -37,11 +37,42 @@ async function getAllCat() {
 
 function openModal(id = null) {
     modal.classList.add("active")
-    
+
 
 }
 function closeModal() {
     modal.classList.remove("active")
 }
+function uploadImage() {
+    event.preventDefault()
+    console.log(img.files);
 
+    console.log(img.files[0].size);
+    var checkSize = img.files[0].size / 1024 / 1024;
+    console.log(checkSize);
+    if (checkSize > 2) {
+        alert("please select image less then 2 mb");
+    } else {
+        const formdata = new FormData();
+        formdata.append("file", img.files[0]);
+        formdata.append("upload_preset", "name");
+
+        const requestOptions = {
+            method: "POST",
+            body: formdata,
+            redirect: "follow",
+        };
+
+        fetch(
+            "https://api.cloudinary.com/v1_1/cloudname/image/upload",
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.secure_url);
+                userImageUrl = data.secure_url;
+            })
+            .catch((error) => console.error(error));
+    }
+}
 getAllCat()
