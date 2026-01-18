057f5d3f117a54553ad2195572646de0910edc1e
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2026-01-18 16:43:32 +0500

    i

--- ecom-Admin-Dshboard/product.js
+++ ecom-Admin-Dshboard/product.js
@@ -1,17 +1,44 @@
 document.getElementById("sidebar-container").innerHTML = getSidebar();
+var modal = document.getElementById("modal")
+var catSelect = document.getElementById('prod-cat');
+
+
+
 
 async function getAllCat() {
-    
+    var img = document.getElementById("img")
+    var imgShow = document.getElementById("imgShow")
+    img.addEventListener("change", function () {
+        console.log(img.files[0])
+        var imgFile = img.files[0]
+        var imgUrl = URL.createObjectURL(imgFile)
+        imgShow.src = imgUrl
+        imgShow.style.display = "block"
+    })
     await firebase.database().ref("CATGEORY").get()
-    .then((snap)=>{
-        console.log(snap.val())
-        allCat = Object.values(snap.val())
-        console.log(allCat)
-    })
-    .catch((e) =>{
-        console.log(e)
-    })
+        .then((snap) => {
+            console.log(snap.val())
+            if (snap.val() == null) {
+                return
+            }
+            allCat = Object.values(snap.val())
+            console.log(allCat)
+            for(var i = 0;i<allCat.length ; i++){
+                console.log(allCat[i])
+            }
+        })
+        .catch((e) => {
+            console.log(e)
+        })
 }
 
+function openModal(id = null) {
+    modal.classList.add("active")
+    
+
+}
+function closeModal() {
+    modal.classList.remove("active")
+}
 
 getAllCat()
